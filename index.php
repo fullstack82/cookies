c21450cdacd2743f2bb785e051371c97b56c2be1
fullstack82 <fullstack8210@gmail.com>
2021-01-31 16:01:10 +0100

    Cookies php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php 
+
+/* COOKIES: 
+PHP tiene soporte para las «cookies» de HTTP de forma transparente. Las cookies son un mecanismo por el que se almacenan datos
+en el navegador remoto para monitorizar o identificar a los usuarios que vuelvan al sito web. Las cookies se pueden configurar
+con las funciones setcookie() o setrawcookie().
+*/
