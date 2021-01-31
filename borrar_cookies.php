b72ae185fd6a67e7267ce75c430ac3331aea1d6c
fullstack82 <fullstack8210@gmail.com>
2021-01-31 16:40:12 +0100

    explicacion

--- borrar_cookies.php
+++ borrar_cookies.php
@@ -1,6 +1,6 @@
 <?php
 
-// Borrar cookies y ponerles caducidad.
+// Borrar cookies y ponerles caducidad. Para borrar una cookie tienes que ponerle caducidad, sino no se borrara;
 if($_COOKIE['micookie']){
     unset($_COOKIE['micookie']);
     setcookie('micookie', '', time() -100 );
