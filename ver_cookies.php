fb56ed064bdd90211784f53e1e2e088041deaf8c
fullstack82 <fullstack8210@gmail.com>
2021-01-31 16:25:59 +0100

    creación de cookies

--- ver_cookies.php
+++ ver_cookies.php
@@ -3,3 +3,18 @@
 /* Para mostrar el valor de las cookies, tengo que usar $_COOKIE, una variable superglobal,
 * es un array asociativo.
 */
+// Comprueba que existe la cookie (isset), y si existe me la muestra por pantalla;
+if(isset($_COOKIE['micookie'])){
+    echo "<h1>".$_COOKIE['micookie']."</h1>";
+}else{
+    echo 'no existe la cookie';
+}
+
+
+
+
+if(isset($_COOKIE['unyear'])){
+    echo "<h1>".$_COOKIE['unyear']."</h1>";
+}else{
+    echo 'No existe la cookie';
+}
